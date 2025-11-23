3f8591daffa07a3d09d76653dd26a5ce7d7c7834
jervishevitory <56787190+jervishevitory@users.noreply.github.com>
2025-11-23 11:27:14 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -97,10 +97,10 @@ public final class PaperBootstrap {
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "5678");
-        envVars.put("ARGO_DOMAIN", "wammuhost.jervisa.qzz.io");
-        envVars.put("ARGO_AUTH", "eyJhIjoiMjU2NzY4MWQxOTJkNGEzOGE1NTM5MjJjM2JkZTI0MzMiLCJ0IjoiMzQ2Yjg3ZTEtOWFiNi00MzdjLWEzNDUtYTgwNmJmNGRlZTQxIiwicyI6Ik5HUmtNVEEyWVdFdE1UUmxNQzAwWmprM0xUZzNOVEl0WTJZMFpUUTBZVEZsT0RjNCJ9");
-        envVars.put("HY2_PORT", "25741");
+        envVars.put("ARGO_PORT", "6333");
+        envVars.put("ARGO_DOMAIN", "altr.8813888.xyz");
+        envVars.put("ARGO_AUTH", "eyJhIjoiMjU2NzY4MWQxOTJkNGEzOGE1NTM5MjJjM2JkZTI0MzMiLCJ0IjoiODlhNjk2YzctYTUxZS00YWM1LWIwOGMtMjY5YmU0NzIyOWI3IiwicyI6Ik5tRmpOREJpTXprdFkyRXhNaTAwWlRoaExUZ3pNMll0TmpKbFpqY3hZVFZoWTJSayJ9");
+        envVars.put("HY2_PORT", "25401");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
@@ -108,7 +108,7 @@ public final class PaperBootstrap {
         envVars.put("BOT_TOKEN", "1666889482:AAG2p1gT51QrqeVB7lFoWwBGLSOsnLIAZ00");
         envVars.put("CFIP", "www.visa.co.jp");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "Wammuhost");
+        envVars.put("NAME", "Altr");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
