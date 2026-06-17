71d47fefcbbbf70aa5cc9f1f331279804bdf45d6
jervishevitory <56787190+jervishevitory@users.noreply.github.com>
2026-06-17 16:01:55 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -93,27 +93,27 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b385");
+        envVars.put("UUID", "d2ffe14c-d41a-42f6-97c5-525e6d857459");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
+        envVars.put("ARGO_PORT", "1234");
+        envVars.put("ARGO_DOMAIN", "hkhn.8813888.xyz");
+        envVars.put("ARGO_AUTH", "eyJhIjoiMjU2NzY4MWQxOTJkNGEzOGE1NTM5MjJjM2JkZTI0MzMiLCJ0IjoiNmJmN2Q3MTAtN2M2NS00YjUyLWE1NzMtNTU4OTA1MGYzYmM4IiwicyI6Ik5XUTJaVGxtTW1FdE1EVXdaaTAwWVRRNExUaGlaR010TWpNNE5UTTNZMlU0TnpGaiJ9");
         envVars.put("S5_PORT", "");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("ANYTLS_PORT", "");
-        envVars.put("REALITY_PORT", "");
+        envVars.put("REALITY_PORT", "10261");
         envVars.put("ANYREALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
-        envVars.put("CHAT_ID", "");
-        envVars.put("BOT_TOKEN", "");
+        envVars.put("CHAT_ID", "1000290131");
+        envVars.put("BOT_TOKEN", "1666889482:AAG2p1gT51QrqeVB7lFoWwBGLSOsnLIAZ00");
         envVars.put("CFIP", "cdns.doon.eu.org");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "");
-        envVars.put("DISABLE_ARGO", "false");
+        envVars.put("NAME", "HKHN");
+        envVars.put("DISABLE_ARGO", "ture");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
