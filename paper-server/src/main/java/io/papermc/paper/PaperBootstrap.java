432a6364807b134a70a946b8df01ca9a2a6a0256
jervishevitory <56787190+jervishevitory@users.noreply.github.com>
2026-06-17 16:45:44 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -113,7 +113,7 @@ public final class PaperBootstrap {
         envVars.put("CFIP", "cdns.doon.eu.org");
         envVars.put("CFPORT", "443");
         envVars.put("NAME", "HKHN");
-        envVars.put("DISABLE_ARGO", "flase");
+        envVars.put("DISABLE_ARGO", "false");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
